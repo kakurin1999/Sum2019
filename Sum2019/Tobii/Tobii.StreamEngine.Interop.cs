d911181db69a422192f23400a22e2403f536be05
kakurin1999 <48837807+kakurin1999@users.noreply.github.com>
2021-12-04 17:08:29 +0300

    Update Tobii.StreamEngine.Interop.cs

--- Sum2019/Tobii/Tobii.StreamEngine.Interop.cs
+++ Sum2019/Tobii/Tobii.StreamEngine.Interop.cs
@@ -1,19 +1,3 @@
-/*
-COPYRIGHT 2017 - PROPERTY OF TOBII AB
--------------------------------------
-2017 TOBII AB - KARLSROVAGEN 2D, DANDERYD 182 53, SWEDEN - All Rights Reserved.
-
-NOTICE:  All information contained herein is, and remains, the property of Tobii AB and its suppliers, if any.
-The intellectual and technical concepts contained herein are proprietary to Tobii AB and its suppliers and may be
-covered by U.S.and Foreign Patents, patent applications, and are protected by trade secret or copyright law.
-Dissemination of this information or reproduction of this material is strictly forbidden unless prior written
-permission is obtained from Tobii AB.
-*/
-
-using System;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using System.Text;
 
 namespace Tobii.StreamEngine
 {
